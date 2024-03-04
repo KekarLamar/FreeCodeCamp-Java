3e8efffb3434a4e3a874dd9208cf5e48aa4b7060
KekarLamar <jcarlos.demarcos@gmail.com>
2024-03-04 08:32:38 +0100

    Arreglado el scanner

--- Tutorial.java
+++ Tutorial.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Tutorial {
-    public static void main(String[] args) {
-      System.out.println("Hola Mundo");
+
+    
+public static String hello ()
+{
+System.out.println("What's your name?");
+    Scanner scan = new Scanner(System.in);
+    String name1 = scan.nextLine();
+return name1;
+}
+
+    public static void main(String[] args) 
+    {
+        String name = hello();
+      System.out.println("Hola " + name);
     }   
   }
